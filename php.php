2eec48ae66ea4cc2848342de8f90a42fe7a802fa
xkendx <xkendx@users.noreply.github.com>
2021-02-17 22:05:24 +0100

    feat: create php file

---
+++ php.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        // echo "Merhaba.<br>";
+        // // echo "<br>";
+        // print("yazı.<br>");
+        // // echo "<br>";
+        // print("merhaba");   
+        
+        // VARIABLES
+        // $variableName
+        $coding = 25;
+
+        $design = 55;
+
+        echo $coding.$design;
+        echo "<br>";
+        echo "$coding $design";
+
+        // CAN'T CREATE VAR
+        // $1var = 5; // it is rise an error
+
+
+        // comment
+        # comment
+
+        
+
+    ?>
+</body>
+</html>
